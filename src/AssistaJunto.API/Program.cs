800032292b42656eea0785e132ad7ffb7ac20bc5
Matheus Rodrigues <matheusrodrigueserb@outlook.com>
2026-02-25 03:39:44 -0300

    ajustes finos no dockercompose e adicionado rodar a migration automáticamente assim que o app é iniciado.

--- src/AssistaJunto.API/Program.cs
+++ src/AssistaJunto.API/Program.cs
@@ -92,6 +92,21 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Ocorreu um erro ao aplicar as migrations no banco de dados.");
+    }
+}
+
 if (app.Environment.IsDevelopment())
     app.UseHttpsRedirection();
 
